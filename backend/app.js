8cdb3846aa3efe5aeb120f5f54511bd5ccefdad6
RajneeshYadav-123 <rajneeshyadav0507@gmail.com>
2026-05-26 15:05:32 +0530

    chane

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,9 @@ app.use(express.json());
 
 app.get("/temperature", (req, res) => {
   console.log("Metro");
+  res.status(200).json({
+    message: "Temperature endpoint working"
+  });
 
 
   // res.send("Temperature Received");
